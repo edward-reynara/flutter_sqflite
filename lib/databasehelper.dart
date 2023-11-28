716ae8dc5826daf00c20a7b2ded4756d032d6a91
edward reynara <edwardreynara@gmail.com>
2023-11-28 16:48:30 +0700

    save db internal storage

--- lib/databasehelper.dart
+++ lib/databasehelper.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
@@ -24,8 +25,24 @@ class SqfliteDatabaseHelper {
   }
 
   Future<Database> initDb()async{
-    Directory directory = await getApplicationDocumentsDirectory();
-    String dbPath = join(directory.path,'syncdatabase.db');
+    await _checkStoragePermissions();
+
+    // Create the directory
+    // Directory externalStorage = await getExternalStorageDirectory();
+    final externalStorage = Directory('/storage/emulated/0');
+    final edoPath = join(externalStorage.path, 'edo');
+
+    // Check if the directory exists
+    if (!(await Directory(edoPath).exists())) {
+      // Create the directory if it doesn't exist
+      await Directory(edoPath).create(recursive: true);
+
+      print('Edo directory created at: $edoPath');
+    }
+
+    ////
+    String dbPath = join(edoPath,'syncdatabase.db');
+    print("dbPath");
     print(dbPath);
     var openDb = await openDatabase(dbPath,version: _version,
     onCreate: (Database db,int version)async{
@@ -46,4 +63,89 @@ class SqfliteDatabaseHelper {
     return openDb;
   }
 
+  Future<void> _checkStoragePermissions() async {
+    // Check if the user has granted the WRITE_EXTERNAL_STORAGE permission.
+    final writePermission = await Permission.storage.status;
+    if (writePermission.isDenied) {
+      // Request the WRITE_EXTERNAL_STORAGE permission.
+      final status = await Permission.storage.request();
+      if (status.isGranted) {
+        // Permission granted, proceed with the operation.
+        print('Write permission granted');
+      } else if (status.isPermanentlyDenied) {
+        // User has permanently denied the permission, show a message.
+        print('Write permission permanently denied');
+        openAppSettings();
+      } else {
+        // Permission denied, show a message.
+        print('Write permission denied');
+      }
+    } else {
+      // Permission already granted, proceed with the operation.
+      print('Write permission already granted');
+    }
+
+    // Check if the user has granted the READ_EXTERNAL_STORAGE permission.
+    final readPermission = await Permission.accessMediaLocation.status;
+    if (readPermission.isDenied) {
+      // Request the READ_EXTERNAL_STORAGE permission.
+      final status = await Permission.accessMediaLocation.request();
+      if (status.isGranted) {
+        // Permission granted, proceed with the operation.
+        print('Read permission granted');
+      } else if (status.isPermanentlyDenied) {
+        // User has permanently denied the permission, show a message.
+        print('Read permission permanently denied');
+        openAppSettings();
+      } else {
+        // Permission denied, show a message.
+        print('Read permission denied');
+      }
+    } else {
+      // Permission already granted, proceed with the operation.
+      print('Read permission already granted');
+    }
+
+    // Check2
+    final readPermission2 = await Permission.mediaLibrary.status;
+    if (readPermission2.isDenied) {
+      // Request the READ_EXTERNAL_STORAGE permission.
+      final status = await Permission.mediaLibrary.request();
+      if (status.isGranted) {
+        // Permission granted, proceed with the operation.
+        print('Read permission granted');
+      } else if (status.isPermanentlyDenied) {
+        // User has permanently denied the permission, show a message.
+        print('Read permission permanently denied');
+        openAppSettings();
+      } else {
+        // Permission denied, show a message.
+        print('Read permission denied');
+      }
+    } else {
+      // Permission already granted, proceed with the operation.
+      print('Read permission already granted');
+    }
+
+    // Check3
+    final readPermission3 = await Permission.manageExternalStorage.status;
+    if (readPermission3.isDenied) {
+      // Request the READ_EXTERNAL_STORAGE permission.
+      final status = await Permission.manageExternalStorage.request();
+      if (status.isGranted) {
+        // Permission granted, proceed with the operation.
+        print('Read permission granted');
+      } else if (status.isPermanentlyDenied) {
+        // User has permanently denied the permission, show a message.
+        print('Read permission permanently denied');
+        openAppSettings();
+      } else {
+        // Permission denied, show a message.
+        print('Read permission denied');
+      }
+    } else {
+      // Permission already granted, proceed with the operation.
+      print('Read permission already granted');
+    }
+  }
 }
